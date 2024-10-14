66861449f757a66d7d2a6a04e9e9e6298e2e6cbd
ml1413 <ml1413sam@gmail.com>
2024-10-14 11:02:41 +0300

    refactor

--- app/src/main/java/com/my/a15/MainActivity.kt
+++ app/src/main/java/com/my/a15/MainActivity.kt
@@ -1,13 +1,11 @@
 package com.my.a15
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectDragGestures
@@ -22,9 +20,7 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -33,7 +29,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalDensity
@@ -41,6 +37,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.my.a15.ui.theme._15Theme
+import com.my.a15.ui.theme.colorCorrectPosition
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -55,11 +52,12 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
 @Composable
 fun Greeting(modifier: Modifier = Modifier, paddingValues: PaddingValues = PaddingValues()) {
-    Log.d("TAG1", "Greeting: ")
     val list = remember { mutableStateOf((listOf(null) + (1..15)).shuffled()) }
-
+    val listReference = (1..16).toList()
+    var indexNull: Int? = null
     val itemWidth = remember { mutableStateOf(0) }
     var width = 0
     val indexSwipeItem = remember { mutableStateOf(0) }
@@ -76,6 +74,7 @@ fun Greeting(modifier: Modifier = Modifier, paddingValues: PaddingValues = Paddi
             }
         }
     )
+
 
     Box(
         modifier = modifier
@@ -105,37 +104,41 @@ fun Greeting(modifier: Modifier = Modifier, paddingValues: PaddingValues = Paddi
                                 width = layoutCoordinates.size.width
                             }
                             .pointerInput(Unit) {
-                                var indexNull = -1
                                 // input swipe
                                 detectDragGestures { _, dragAmount ->
-                                    indexNull = if (indexNull < 1) {
-                                        list.value.indexOf(null)
-                                    } else -1
+                                    indexNull =
+                                        if (indexNull == null) list.value.indexOf(null) else null
 
-                                    if (dragAmount.x > 0 && indexNull == itemIndex + 1) {
-                                        isHorizontal.value = true
-                                        indexSwipeItem.value = itemIndex
-                                        indexEmptyItem.value = itemIndex + 1
-                                        itemWidth.value = width
-                                    } else if (dragAmount.x < 0 && indexNull == itemIndex - 1) {
-                                        isHorizontal.value = true
-                                        indexSwipeItem.value = itemIndex
-                                        indexEmptyItem.value = itemIndex - 1
-                                        itemWidth.value = -width
+                                    when {
+                                        dragAmount.x > 0 && indexNull == itemIndex + 1 -> {
+                                            isHorizontal.value = true
+                                            indexSwipeItem.value = itemIndex
+                                            indexEmptyItem.value = itemIndex + 1
+                                            itemWidth.value = width
+                                        }
 
-                                    } else
-                                        if (dragAmount.y > 0 && indexNull == itemIndex + 4) {
+                                        dragAmount.x < 0 && indexNull == itemIndex - 1 -> {
+                                            isHorizontal.value = true
+                                            indexSwipeItem.value = itemIndex
+                                            indexEmptyItem.value = itemIndex - 1
+                                            itemWidth.value = -width
+                                        }
+
+                                        dragAmount.y > 0 && indexNull == itemIndex + 4 -> {
                                             isHorizontal.value = false
                                             indexSwipeItem.value = itemIndex
                                             indexEmptyItem.value = itemIndex + 4
                                             itemWidth.value = width
-                                        } else if (dragAmount.y < 0 && indexNull == itemIndex - 4) {
+                                        }
+
+                                        dragAmount.y < 0 && indexNull == itemIndex - 4 -> {
                                             isHorizontal.value = false
                                             indexSwipeItem.value = itemIndex
                                             indexEmptyItem.value = itemIndex - 4
                                             itemWidth.value = -width
-
                                         }
+                                    }
+
                                 }
                             }
                             .padding(8.dp)
@@ -162,7 +165,10 @@ fun Greeting(modifier: Modifier = Modifier, paddingValues: PaddingValues = Paddi
 
                             Text(
                                 text = it.toString(),
-                                color = MaterialTheme.colorScheme.background,
+                                color = if (item == listReference[itemIndex])
+                                    colorCorrectPosition
+                                else
+                                    MaterialTheme.colorScheme.background,
                                 fontWeight = FontWeight.ExtraBold,
                                 style = MaterialTheme.typography.displaySmall
                             )
@@ -188,14 +194,4 @@ fun <T> List<T>.swap(index1: Int, index2: Int): List<T> {
     val newList = this.toMutableList()
     newList[index1] = newList[index2].also { newList[index2] = newList[index1] }
     return newList.toList()
-}
-
-private fun checkPeripheryNum(value: Int, list: List<Int?>): Boolean {
-    val index = list.indexOf(null)
-    val leftNum = if (index % 4 == 0) null else list[index - 1]
-    val rightNum = if ((index + 1) % 4 == 0) null else list[index + 1]
-    val upNum = if (index < 4) null else list[index - 4]
-    val bottomNum = if (index + 4 > list.lastIndex) null else list[index + 4]
-    val listNum = listOf(leftNum, rightNum, upNum, bottomNum)
-    return listNum.contains(value)
 }
